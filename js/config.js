39922266e0ff04caf3cbc625ecbb34b5a7e5b2ec
tsvalencio-IA <tsvalencio@gmail.com>
2026-09-25 17:00:11 -0300

    Marca versão V15.22.1

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 window.CHECKLIST_APP = {
-  version: '15.22.0',
+  version: '15.22.1',
   appName: 'OFICIN-IA Checklist',
   appId: 'br.com.thiaguinhosolucoes.oficinia.checklist.v15githubpages',
   footer: 'Powered by thIAguinho Soluções Digitais',
