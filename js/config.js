8ec4a86a02fb692b464015a9491f2ec211513ece
tsvalencio-IA <tsvalencio@gmail.com>
2026-09-25 15:42:58 -0300

    Atualiza versão da integração SAAS-2 para V15.21.1

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 window.CHECKLIST_APP = {
-  version: '15.21.0',
+  version: '15.21.1',
   appName: 'OFICIN-IA Checklist',
   appId: 'br.com.thiaguinhosolucoes.oficinia.checklist.v15githubpages',
   footer: 'Powered by thIAguinho Soluções Digitais',
